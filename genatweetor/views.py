d81ab9e543178ee2e8f1000ddcb40b5622a38585
SaifAbrahimSaleem <37380757+SaifAbrahimSaleem@users.noreply.github.com>
2019-01-15 01:16:10 +0000

    Update views.py

--- genatweetor/views.py
+++ genatweetor/views.py
@@ -52,6 +52,7 @@ def login(request):
     request.session['oauth_token_secret'] = auth['oauth_token_secret']
     #Redirect the user to the url obtained by twitter
     redirect_url = auth['auth_url']
+    print(redirect_url)
     # context = {
     #     'responseMessage' : "Successfully Logged in!"
     # }
