f1671d1a255c5342d4d3fca16539b69993bb46f3
SaifAbrahimSaleem <37380757+SaifAbrahimSaleem@users.noreply.github.com>
2019-01-15 01:51:12 +0000

    Update views.py

--- genatweetor/views.py
+++ genatweetor/views.py
@@ -1,40 +1,11 @@
 from django.conf import settings #then do settings.theVariableYouWantToAccess
 from django.shortcuts import render, redirect, reverse
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from twython import Twython, TwythonError
 import json
 # Create your views here.
 
 appname = 'genatweetor'
-#NEEDS TO BE LOCAL HOST
-#CALLBACK_URL = 'https://genatweetor.herokuapp.com/dashboard'
-
-# LOGGED IN DECORATOR FOR THE PROGRAM
-# def is_loggedIn(view):
-#     def logged_InView(request):
-#         if 'username' in request.session:
-#         username = request.session['username']
-#         try:
-
-        #PROGRAMMING LOGIC
-        # IF THE USERNAME or email IS IN REQUEST.SESSION
-        # QUERY THE USERNAME AND THE PASSWORD WITH TWITTER
-        # RETURN SUCCESS
-        #
-        # ELSE
-        # DISPLAY TWITTER ERROR
-
-# def authenticateConnection(request, user):
-#     OAUTH_TOKEN = settings.auth['oauth_token']
-#     OAUTH_TOKEN_SECRET = settings.auth['oauth_token_secret']
-#     authorisedURL = settings.auth['auth_url']
-#       redirect(authorisedURL)
-########## HANDLING CALLBACK ##########
-#     oauth_verifier = request.GET['oauth_verifier']
-#     twitter = Twython(settings.APP_KEY, settings.APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-#     final_step = twitter.get_authorized_tokens(oauth_verifier)
-#     OAUTH_TOKEN = final_step['oauth_token']
-#     OAUTH_TOKEN_SECRET = final_step['oauth_token_secret']
 
 #LOGIN VIEW
 def index(request):
@@ -66,7 +37,7 @@ def dashboard(request):
 
     oauth_verifier = request.session.get('oauth_verifier')
     if not oauth_verifier:
-        abort(401, 'missing oauth_verifier')
+        raise Http404('missing oauth_verifier')
 
     twitter = Twython(settings.CONSUMER_KEY, settings.CONSUMER_SECRET,
                       request.session['oauth_token'], request.session['oauth_token_secret'])
