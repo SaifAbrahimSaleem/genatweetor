173c971b7e9b18baeed96c3ca8e555dbdd419439
SaifAbrahimSaleem <37380757+SaifAbrahimSaleem@users.noreply.github.com>
2019-05-16 14:21:22 +0100

    Update settings.py

--- FinalYearProject/settings.py
+++ FinalYearProject/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = '^$)g*kvy#cp15s5w5oz(eh5^trs9wtwfxgyiz97x4kqmr!%1b$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://genatweetor.herokuapp.com']
 
 # Error Logging
 
